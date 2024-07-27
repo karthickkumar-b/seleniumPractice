8a9fc4ecbcb658a42105a4bf3593639e5a840ceb
Karthickkumarb <Karthickkumarb04@gmail.com>
2024-07-27 13:05:52 +0530

    Testing for Searching in google engine is done

--- src/test/java/SearchInGoogle.java
+++ src/test/java/SearchInGoogle.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class SearchInGoogle {
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SearchInGoogle {
+
+    WebDriver webDriver;
+
+    public void launchBrowser(){
+        System.setProperty("webDriver.chrome.driver","C:/Users/karth/Downloads/chromedriver-win64");
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.get("https://www.google.com/");
+    }
+
+    public void searchProduct() throws InterruptedException {
+        Thread.sleep(1000);
+        WebElement webElement = webDriver.findElement(By.id("APjFqb"));
+        webElement.sendKeys("flipkart.com");
+        webElement.sendKeys(Keys.ENTER);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SearchInGoogle searchInGoogle = new SearchInGoogle();
+        searchInGoogle.launchBrowser();
+        Thread.sleep(1000);
+        searchInGoogle.searchProduct();
+    }
 }
